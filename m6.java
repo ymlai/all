8cc38d4db4b784e366ae0430c76c8fdfce84f214
ymlai <ymlai529@gmail.com>
2016-01-11 01:11:59 +0800

    Update m6.java

--- m6.java
+++ m6.java
@@ -12,8 +12,9 @@ public class marks6 {
 		TreeSet<Integer> current = new TreeSet<Integer>();
 		LinkedList<Double> current_ave = new LinkedList<Double>();
 		Boolean loop = true;
+		double mean = 0, sd=0;
 		long n=0,  n_win=0, current_n=0;
-		for (int i=0; i<100 ;i++){
+		for (int i=0; i<10 ;i++){
 			loop=true;
 			current = genFunction();
 			target = genFunction();
@@ -22,48 +23,46 @@ public class marks6 {
 			    current_n+=1;
 			    if (not_win(current ,target)){
 			    		current = genFunction();
-			    		//target = genFunction();
-			    		if (n%10000000==0 && n !=0){
-			    			//System.out.println(n);
-				    		}
 			    }
 			    if (!not_win(current ,target)){
-				//System.out.println(current);
-				//System.out.println(target);				
+				System.out.println(current+"="+target);				
 				n_win+=1;
 				System.out.println("no of winner:" +n_win);
-				//System.out.println("current_ave"+ n/n_win);
 				loop=false;
 				System.out.println("current_n:"+current_n);
 				current_ave.add((double) current_n);
-				//System.out.println("current_ave_size 1:"+current_ave.size());
+				mean=n/n_win;
+				System.out.println("mean:"+mean);
+				sd = Math.sqrt( get_var(mean, current_ave )  );
+				System.out.println("sd:"+sd);
 				current_n=0;
 				}
 			}
 		}
-		System.out.println("current_ave_size 2:"+current_ave.size());
 		System.out.println("============================");
 		System.out.println("total player:"+n);
 		System.out.println("total no of winner:"+n_win);
-		double sample_mean;
-		double sum_diff_2 = 0;
-		double diff;
-		double diff_2;
-		java.util.Iterator<Double> cur_n = current_ave.iterator();
-		while(cur_n.hasNext()){
-			sample_mean = cur_n.next();	
-			diff = (sample_mean-n/n_win);
-			diff_2= Math.pow(diff,2);
-			sum_diff_2+=diff_2;
-			
-			}
-		
-		System.out.println("mean:"+n/n_win);
-		double var = sum_diff_2/(current_ave.size()-1);
-		double sd = Math.sqrt( var  );
-		//System.out.println(c+"   current_ave.size_3:" /current_ave.size())
+		System.out.println("mean:"+mean);
 		System.out.println("sd:"+sd);
 		}
+	
+	public static double get_var(double mean, LinkedList<Double> current_ave ) {
+		double sample_x;
+		double sum_diff_2 = 0;
+		double diff;
+		double diff_2, var;
+		java.util.Iterator<Double> cur_n = current_ave.iterator();
+		while(cur_n.hasNext()){
+			sample_x = cur_n.next();	
+			diff = (sample_x-mean);
+			diff_2= Math.pow(diff,2);
+			sum_diff_2+=diff_2;
+			}
+		var = sum_diff_2/(current_ave.size()-1);
+		return var;
+	}
+
+	
 	public static TreeSet<Integer> genFunction() {
 		Random rng = new Random(); // Ideally just create one instance globally
 		// Note: use LinkedHashSet to maintain insertion order
